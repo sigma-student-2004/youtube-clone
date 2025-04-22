ab53cd510e07f7a3a1d34c6954b0887382c542d6
sigma-student-2004 <mohitkumawat1901@gmail.com>
2025-04-22 14:50:49 +0530

    add file

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar=()=>{
+    return(
+        <div>
+            <h1>Rohit kumawat</h1>
+        </div>
+    )
+}
+export default Navbar
