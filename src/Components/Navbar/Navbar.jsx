e409ef3e503b50eb46e5a3f0c858ab83c7feb421
sigma-student-2004 <mohitkumawat1901@gmail.com>
2025-04-22 16:06:42 +0530

    add file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Navbar.css";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/profile.png";
+
 const Navbar=()=>{
     return(
-        <div>
-            <h1>Rohit kumawat</h1>
-        </div>
+        <nav className="flex-div">
+           <div className="nav-left flex-div">
+                <img className="menu-icon"  src={menu_icon} alt="" />
+                 <img className="logo" src={logo} alt="" />
+            </div>
+            <div className="nav-middle flex div">
+                <input type="text" placeholder="Search" />
+                <img src={search_icon} alt="" />
+            </div>
+            <div className="nav-right">
+                <img src={upload_icon} alt="" />
+                <img src={more_icon} alt="" />
+                <img src={notification_icon} alt="" />
+                <img src={profile_icon} alt="" />
+            </div> 
+            
+        </nav>
     )
 }
 export default Navbar
